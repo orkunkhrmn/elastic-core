2447e8a9ad375726161f38060f805a932328e5d1
orkunkhrmn <orkunkahraman@gmail.com>
2023-06-03 12:07:24 +0300

    elastic settings added

---
+++ elastic-core/Extensions/ElasticSearhExtensions.cs
@@ -0,0 +1,29 @@
+﻿using elastic_core.Models;
+using Elasticsearch.Net;
+using Nest;
+
+namespace elastic_core.Extensions
+{
+    public static class ElasticSearhExtensions
+    {
+        public static void AddElasticsearch(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionSettings = new ConnectionSettings(new Uri("http://localhost:9200"))
+                .DefaultIndex("customer");
+
+            var client = new ElasticClient(connectionSettings);
+
+            services.AddSingleton<IElasticClient>(client);
+
+            CreateIndex(client, "customer");
+        }
+
+        private async static void CreateIndex(IElasticClient client, string indexName)
+        {
+            Customer customer = new Customer { Id = Guid.NewGuid(), FirstName = "Orkun", LastName = "Kahraman" };
+
+            var indexResponse = await client.IndexAsync(customer, idx => idx.Index(indexName).OpType(OpType.Index));
+        }
+    }
+}
---
+++ elastic-core/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace elastic_core.Models
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+    }
+}
